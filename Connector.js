73f1ac349bd251b084a006d012f85cdd9f62532c
BK <basknappers@gmail.com>
2021-09-04 15:57:03 +0200

    Added Functions

--- Connector.js
+++ Connector.js
@@ -70,16 +70,77 @@ class Connector{
       })
     }
 
+    TransactionsGet(PubKey,Offset=0, Index=10){
+      return new Promise(resolve => {
+          resolve(this.client.TransactionsGet(base58tobytes(PubKey),Offset,Index));
+      })
+    }
+
+    DeploySmart(PubKey,Offset=0, Index=10){
+      return new Promise(resolve => {
+          resolve(this.client.TransactionsGet(base58tobytes(PubKey),Offset,Index));
+      })
+    }
+    
+    SendTransaction(PubKey,Offset=0, Index=10){
+      return new Promise(resolve => {
+          resolve(this.client.TransactionsGet(base58tobytes(PubKey),Offset,Index));
+      })
+    }
+
+    SyncState(){
+      return new Promise(resolve => {
+          resolve(this.client.SyncStateGet());
+      })
+    }
+
+    ActualFeeGet(TransactionSize){
+      return new Promise(resolve => {
+          resolve(this.client.ActualFeeGet(TransactionSize));
+      })
+    }
+
+    WalletIdGet(Address){
+      return new Promise(resolve => {
+          resolve(this.client.WalletIdGet(base58tobytes(Address)));
+      })
+    }
+
+    PoolListGetStable(Sequence=0, Limit=10){
+      return new Promise(resolve => {
+          resolve(this.client.PoolListGetStable(Sequence, Limit));
+      })
+    }
+
+    PoolListGet(Offset=0, Limit=10){
+      return new Promise(resolve => {
+          resolve(this.client.PoolListGet(Offset, Limit));
+      })
+    }
+
+    PoolInfoGet(Offset=0, Limit=10){
+      return new Promise(resolve => {
+          resolve(this.client.PoolInfoGet(Offset, Limit));
+      })
+    }
+
+
 
 }
 
 
 
-var connect_ = new Connector("207.244.228.131", 9091);
+var connect_ = new Connector("165.22.212.253", 9090);
 //connect_.TransactionsListGet(0,10).then((res) => {connect_.Close();console.log(res); });
 //connect_.TransactionGet(63192736,0).then((res) => {connect_.Close();console.log(res); });
 //connect_.WalletGetBalance("Ba2Uhtcy4cZ6nJNaQKXWcdLgPH7Kat2R67CUkRU2bkTk").then((res) => {connect_.Close();console.log(res); });
 //connect_.WalletTransactionsCountGet("Ba2Uhtcy4cZ6nJNaQKXWcdLgPH7Kat2R67CUkRU2bkTk").then((res) => {connect_.Close();console.log(res); });
 //connect_.WalletDataGet("Ba2Uhtcy4cZ6nJNaQKXWcdLgPH7Kat2R67CUkRU2bkTk").then((res) => {connect_.Close();console.log(res); });
-connect_.StatsGet().then((res) => {connect_.Close();console.log(res); });
- 
+//connect_.StatsGet().then((res) => {connect_.Close();console.log(res); });
+//connect_.TransactionsGet("ACkyon3ERkNEcNwpjUn4S6TLP3L76LFg3X6kWoJx82dK").then((res) => {connect_.Close();console.log(res); }); 
+//connect_.SyncState().then((res) => {connect_.Close();console.log(res); });
+//connect_.ActualFeeGet().then((res) => {connect_.Close();console.log(res); });
+//connect_.WalletIdGet("ACkyon3ERkNEcNwpjUn4S6TLP3L76LFg3X6kWoJx82dK").then((res) => {connect_.Close();console.log(res); });
+//connect_.PoolListGetStable(0,10).then((res) => {connect_.Close();console.log(res); });
+//connect_.PoolListGet(0,10).then((res) => {connect_.Close();console.log(res); });
+connect_.PoolInfoGet(0,10).then((res) => {connect_.Close();console.log(res); });
