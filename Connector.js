f5ef778f16ac7ada2ff540cb01b2ef3214fedf6f
BK <basknappers@gmail.com>
2021-09-04 18:29:15 +0200

    Update

--- Connector.js
+++ Connector.js
@@ -1,6 +1,7 @@
 var api = require('./API')
 var thrift = require('thrift')
 var ttypes = require('./api_types');
+var general_ttypes = require('./general_types');
 var base58 = require('./bs58')
 const assert = require('assert');
 var transport = thrift.TBufferedTransport;
@@ -27,6 +28,81 @@ class Connector{
         this.client = thrift.createClient(api, this.connection);
     }
 
+    
+
+    fee(v) {
+      let s = v > 0 ? 0 : 1;
+      v = Math.abs(v);
+      let exp = v === 0 ? 0 : Math.log10(v);
+      exp = Math.floor(exp >= 0 ? exp + 0.5 : exp - 0.5);
+      v /= Math.pow(10, exp);
+      if (v >= 1) {
+          v *= 0.1;
+          ++exp;
+      }
+      v = Number((v * 1024).toFixed(0));
+      return { exp: exp + 18, man: v === 1024 ? 1023 : v };
+    }
+
+    numbToBits(int) {
+      let Bits = "";
+      let numb = String(int);
+      while (true) {
+          Bits = (numb % 2) + Bits;
+          numb = Math.floor(numb / 2);
+
+          if (numb <= 1) {
+              Bits = numb + Bits;
+              break;
+          }
+      }
+      return Bits;
+    } 
+    bitsToNumb(Bits) {
+      let numb = 0;
+      let mnoj = 1;
+      for (var i = Bits.length - 1; i > 0; i -= 1) {
+          if (Bits[i] !== 0) {
+              numb += mnoj * Bits[i];
+          }
+          mnoj *= 2;
+      }
+      return numb;
+    }
+
+    numbToByte(numb, CountByte) {
+      let InnerId = new Uint8Array(CountByte);
+      numb = String(numb);
+      let i = 1;
+      let index = 0;
+      while (true) {
+          InnerId[index] += (numb % 2) * i;
+          numb = Math.floor(numb / 2);
+          if (numb === 0) {
+              break;
+          }
+          if (numb === 1) {
+              var b = (numb % 2) * i * 2;
+              if (b === 256) {
+                  ++InnerId[index + 1];
+              } else {
+                  InnerId[index] += (numb % 2) * i * 2;
+              }
+              break;
+          }
+
+          if (i === 128) {
+              i = 1;
+              index++;
+          } else {
+              i *= 2;
+          }
+      }
+      return InnerId;
+    }
+
+    
+
     Close(){
         this.connection.end();
     }
@@ -36,7 +112,9 @@ class Connector{
             resolve(this.client.TransactionsListGet(Offset,Limit));
         })
     }
+    transfer_coins(){
 
+    }
     TransactionGet(PoolSeq,Index){
       var txsid = new ttypes.TransactionId();
       txsid.poolSeq = PoolSeq;
@@ -82,11 +160,7 @@ class Connector{
       })
     }
     
-    SendTransaction(PubKey,Offset=0, Index=10){
-      return new Promise(resolve => {
-          resolve(this.client.TransactionsGet(base58tobytes(PubKey),Offset,Index));
-      })
-    }
+    
 
     SyncState(){
       return new Promise(resolve => {
@@ -236,6 +310,49 @@ class Connector{
           resolve(this.client.TrustedGet(Page));
       })
     }
+    createTransaction(integral, fraction, fee, publickey, privatekey, target, txsid) {
+      return new Promise(resolve => {
+        let res = this.client.WalletTransactionsCountGet(base58tobytes(publickey));
+        var tran = new ttypes.Transaction();
+        tran.id = res.lastTransactionInnerId + 1;
+        tran.source = base58tobytes(publickey);
+        tran.target = base58tobytes(target);
+        tran.amount = new general_ttypes.Amount({integral: integral, fraction: fraction});
+
+        let F = this.fee(fee);
+        let FE = this.numbToBits(F.exp);
+        while (FE.length < 5){
+            FE = "0" + FE;
+        }
+        let FM = this.numbToBits(F.man);
+        while (FM.length < 10) {
+            FM = "0" + FM;
+        }
+
+        tran.fee = new ttypes.AmountCommission({
+            commission: this.bitsToNumb("0" + FE + FM)
+        });
+
+        tran.currency = 1;
+
+        let PerStr = this.numbToByte(tran.id, 6);
+        PerStr = this.concatTypedArrays(PerStr, tran.source);
+        PerStr = this.concatTypedArrays(PerStr, tran.target);
+        PerStr = this.concatTypedArrays(PerStr, this.numbToByte(tran.amount.integral, 4));
+        PerStr = this.concatTypedArrays(PerStr, this.numbToByte(tran.amount.fraction, 8));
+        PerStr = this.concatTypedArrays(PerStr, this.numbToByte(tran.fee.commission, 2));
+        PerStr = this.concatTypedArrays(PerStr, new Uint8Array([1]));
+        PerStr = this.concatTypedArrays(PerStr, new Uint8Array(1));
+        tran.signature = nacl.sign.detached(PerStr, base58tobytes(privatekey));
+        resolve(tran);
+      })
+      
+  }
+  SendTransaction(Integeral,fraction,fee,PublicKey,PrivateKey,Target,UserData=null,TxsID=0,Transaction = null){
+    return new Promise(resolve => {
+        resolve(this.client.TransactionFlow(this.createTransaction(Integeral,fraction,fee,PublicKey,PrivateKey,Target)));
+    })
+  }
 
 
 
@@ -243,7 +360,8 @@ class Connector{
 
 
 
-var connect_ = new Connector("165.22.212.253", 9090);
+var connect_ = new Connector("194.163.152.177", 9091);
+connect_.SendTransaction(0,0,0.1,"5B3YXqDTcWQFGAqEJQJP3Bg1ZK8FFtHtgCiFLT5VAxpe","3rUevsW5xfob6qDxWMDFwwTQCq39SYhzstuyfUGSDvF2QHBRyPD8fSk49wFXaPk3GztfxtuU85QHfMV3ozfqa7rN","7TAp6BWBMJ3Ep6Wayu1TSApXifBFVAoZtCx6fALjo4At").then((res) => {connect_.Close();console.log(res); });
 //connect_.TransactionsListGet(0,10).then((res) => {connect_.Close();console.log(res); });
 //connect_.TransactionGet(63192736,0).then((res) => {connect_.Close();console.log(res); });
 //connect_.WalletGetBalance("Ba2Uhtcy4cZ6nJNaQKXWcdLgPH7Kat2R67CUkRU2bkTk").then((res) => {connect_.Close();console.log(res); });
--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Third Party Library for implementing Credits Blockchain in Node Javascript
 
 ## Getting started
 
+```javascript
+npm i centre-nodejs-credits
+```
 After installing it's time to actually use it. To get started we have to add the Javascript Require:  `const Connector = 'Centre-NodeJS-CS';
 
 Centre is providing an easy way of connecting to the Credits Blockchain where the goal is to limit the time needed for new Developers intergrating the Credits Blockchain into there projects
@@ -18,8 +21,8 @@ https://centr.gitbook.io/netcs
 ## Connector Object
 
 In order to get the library working we first need to initialize the Connector object
-```python
-connect_ = Connector("95.111.224.219:9091");
+```javascript
+var connect_ = new Connector("165.22.212.253", 9090);
 ```
 The Connector object needs to have 2 variables the first: IP address of a node running Credits Blockchain and Second: the API port of that particular node.
 
@@ -27,16 +30,16 @@ The Connector object needs to have 2 variables the first: IP address of a node r
 Retrieving Balance
 --
 For a very easy example we are going to retrieve a Balance of a give wallet address
-```python
-Connect_ =  Connector("165.22.212.253:9090")
-print(Connect_.WalletGetBalance("ACkyon3ERkNEcNwpjUn4S6TLP3L76LFg3X6kWoJx82dK"))
+```javascript
+var connect_ = new Connector("165.22.212.253", 9090);
+connect_.WalletGetBalance("Ba2Uhtcy4cZ6nJNaQKXWcdLgPH7Kat2R67CUkRU2bkTk").then((res) => {connect_.Close();console.log(res); });
 ```
 With only 2 Lines of code we can now retrieve a balance from the blockchain that easy !
 
 Sending Transaction
 --
 In the next example we are going to send an transaction with the native currency CS
-```csharp
+```javascript
 connect_ = new Connector("95.111.224.219:9091");
 Print(connect_.SendTransaction(1, 0, "Enter Sender Publickey", "Enter Sender Privatekey", "Enter Receiver Publickey"));
 ```
